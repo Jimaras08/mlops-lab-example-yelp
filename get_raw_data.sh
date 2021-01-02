41282d87b49a238b587c01864c564cd0b68e048b
Laszlo Sragner <sragner@gmail.com>
2021-01-02 14:06:06 +0000

    adding raw data downloading script

---
+++ get_raw_data.sh
@@ -0,0 +1,2 @@
+wget -O data/raw/yelp_review_full_csv.tgz https://s3.amazonaws.com/fast-ai-nlp/yelp_review_full_csv.tgz
+wget -O data/raw/yelp_review_polarity_csv.tgz https://s3.amazonaws.com/fast-ai-nlp/yelp_review_polarity_csv.tgz
