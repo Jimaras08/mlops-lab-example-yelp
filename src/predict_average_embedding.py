9b04f971e04eadbbe3d5d51938bf2212907bea6e
Paulo Maia <paulo.maia@nilg.ai>
2021-02-01 22:29:51 +0000

    predict script

---
+++ src/predict_average_embedding.py
@@ -0,0 +1,27 @@
+from torchtext.data.utils import ngrams_iterator
+from torchtext.data.utils import get_tokenizer
+
+def predict(text, model, vocab, ngrams):
+    tokenizer = get_tokenizer("basic_english")
+    with torch.no_grad():
+        text = torch.tensor([vocab[token]
+                            for token in ngrams_iterator(tokenizer(text), ngrams)])
+        output = model(text, torch.tensor([0]))
+        return output.argmax(1).item()
+
+ex_text_str = "MEMPHIS, Tenn. – Four days ago, Jon Rahm was \
+    enduring the season’s worst weather conditions on Sunday at The \
+    Open on his way to a closing 75 at Royal Portrush, which \
+    considering the wind and the rain was a respectable showing. \
+    Thursday’s first round at the WGC-FedEx St. Jude Invitational \
+    was another story. With temperatures in the mid-80s and hardly any \
+    wind, the Spaniard was 13 strokes better in a flawless round. \
+    Thanks to his best putting performance on the PGA Tour, Rahm \
+    finished with an 8-under 62 for a three-stroke lead, which \
+    was even more impressive considering he’d never played the \
+    front nine at TPC Southwind."
+
+vocab = train_dataset.get_vocab()
+model = model.to("cpu")
+
+predict(ex_text_str, model, vocab, 1)
