919dd08c676531adcc9364f74aa2eb55e42993b5
Artem Yushkovskiy <ajuszkowski@ya.ru>
2021-02-02 20:48:54 +0300

    Some cleanups and modifications

--- src/predict_average_embedding.py
+++ src/predict_average_embedding.py
@@ -1,5 +1,29 @@
+import torch
+import logging
 from torchtext.data.utils import ngrams_iterator
 from torchtext.data.utils import get_tokenizer
+from torchtext.datasets import text_classification
+from src.average_embedding import TextSentiment
+import pandas as pd
+import numpy as np
+
+logger = logging.getLogger(__file__)
+
+
+class MLflowModel:
+    # TODO
+    def __init__(self, model_path, vocab_path):
+        pass
+    def predict(self, model_input: pd.DataFrame): # -> [np.ndarray | pd.DataFrame]
+        pass
+
+
+def _load_pyfunc(path):
+    return MLflowModel(...)
+
+
+# -- Old code below
+
 
 def predict(text, model, vocab, ngrams):
     tokenizer = get_tokenizer("basic_english")
@@ -9,19 +33,27 @@ def predict(text, model, vocab, ngrams):
         output = model(text, torch.tensor([0]))
         return output.argmax(1).item()
 
-ex_text_str = "MEMPHIS, Tenn. – Four days ago, Jon Rahm was \
-    enduring the season’s worst weather conditions on Sunday at The \
-    Open on his way to a closing 75 at Royal Portrush, which \
-    considering the wind and the rain was a respectable showing. \
-    Thursday’s first round at the WGC-FedEx St. Jude Invitational \
-    was another story. With temperatures in the mid-80s and hardly any \
-    wind, the Spaniard was 13 strokes better in a flawless round. \
-    Thanks to his best putting performance on the PGA Tour, Rahm \
-    finished with an 8-under 62 for a three-stroke lead, which \
-    was even more impressive considering he’d never played the \
-    front nine at TPC Southwind."
+if __name__ == "__main__":
 
-vocab = train_dataset.get_vocab()
-model = model.to("cpu")
+    ex_text_str = "MEMPHIS, Tenn. – Four days ago, Jon Rahm was \
+        enduring the season’s worst weather conditions on Sunday at The \
+        Open on his way to a closing 75 at Royal Portrush, which \
+        considering the wind and the rain was a respectable showing. \
+        Thursday’s first round at the WGC-FedEx St. Jude Invitational \
+        was another story. With temperatures in the mid-80s and hardly any \
+        wind, the Spaniard was 13 strokes better in a flawless round. \
+        Thanks to his best putting performance on the PGA Tour, Rahm \
+        finished with an 8-under 62 for a three-stroke lead, which \
+        was even more impressive considering he’d never played the \
+        front nine at TPC Southwind."
 
-predict(ex_text_str, model, vocab, 1)
+    train_dataset, test_dataset = text_classification.DATASETS['YelpReviewPolarity'](
+            root='../data', ngrams=NGRAMS, vocab=None)
+
+    vocab = train_dataset.get_vocab()
+    VOCAB_SIZE = len(train_dataset.get_vocab())
+    NUN_CLASS = len(train_dataset.get_labels())
+    model = TextSentiment(VOCAB_SIZE, EMBED_DIM, NUN_CLASS)
+    model = model.to("cpu")
+
+    predict(ex_text_str, model, vocab, 1)
--- src/train_average_embedding.py
+++ src/train_average_embedding.py
@@ -1,16 +1,23 @@
-import torch
-import torchtext
 from torchtext.datasets import text_classification
 from torch.utils.data import DataLoader
 import mlflow.pytorch
-from mlflow.tracking import MlflowClient
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks import LearningRateMonitor
 import torch
 import pytorch_lightning as pl
+import pickle
+import logging
 
-from average_embedding import TextSentiment
+from src.average_embedding import TextSentiment
+from src.utils import setup_logging
+import tempfile
+from pathlib import Path
+
+
+logger = logging.getLogger(__file__)
+
+VOCAB_DUMP_PATH = Path(f"{tempfile.mkdtemp()}/vocab.pkl")
+
 
 def generate_batch(batch):
     label = torch.tensor([entry[0] for entry in batch])
@@ -28,9 +35,10 @@ def generate_batch(batch):
 NGRAMS = 1
 BATCH_SIZE = 32
 EMBED_DIM = 32
+MAX_EPOCHS = 1
 
 
-def main(args):
+def main():
 
     train_dataset, test_dataset = text_classification.DATASETS['YelpReviewPolarity'](
         root='../data', ngrams=NGRAMS, vocab=None)
@@ -42,24 +50,35 @@ def main(args):
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True,
                         collate_fn=generate_batch)
 
-    VOCAB_SIZE = len(train_dataset.get_vocab())
+    vocab = train_dataset.get_vocab()
+    VOCAB_SIZE = len(vocab)
     NUN_CLASS = len(train_dataset.get_labels())
     model = TextSentiment(VOCAB_SIZE, EMBED_DIM, NUN_CLASS)
+    model.to(device)
+
+    logger.info(f"Saving vocabulary to {VOCAB_DUMP_PATH}")
+    with open(VOCAB_DUMP_PATH, 'wb') as f:
+        pickle.dump(vocab, f)
 
     early_stopping = EarlyStopping(monitor="val_loss", mode="min", verbose=True, patience=3)
-    checkpoint_callback = ModelCheckpoint(
-        filepath=os.getcwd(), save_top_k=1, verbose=True, monitor="val_loss", mode="min", prefix="",
-    )
     lr_logger = LearningRateMonitor()
     # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.9)
 
     # Loss and optimizer
-    trainer = pl.Trainer(gpus=1, max_epochs=1, progress_bar_refresh_rate=20)
-                    #   callbacks=[lr_logger, early_stopping],
-                    #  checkpoint_callback=True)
+    trainer = pl.Trainer(gpus=1, max_epochs=MAX_EPOCHS, progress_bar_refresh_rate=20,
+                         callbacks=[lr_logger, early_stopping])
 
     # Auto log all MLflow entities
     mlflow.pytorch.autolog(log_models = True)
 
     with mlflow.start_run() as run:
+        logger.info(f"run_id: {run.info.run_id}")
+        mlflow.log_param("max_epochs", MAX_EPOCHS)
+        mlflow.log_artifact(VOCAB_DUMP_PATH, artifact_path="model/data")
+
         trainer.fit(model, train_dataloader, test_dataloader)
+
+
+if __name__ == "__main__":
+    logger = setup_logging()
+    main()
